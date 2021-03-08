96d193a47317b66415749b01a8d1a682e8ce5747
Aleksey Golovenchik <goloven4ik@googlemail.com>
2021-03-08 21:53:52 +0300

    complete

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,20 +1,28 @@
 public class Lambda {
-public static <T>void print_st(T t){
-    System.out.println(t);
-}
-static void printing(SomeInt si){
-    si.print(5);
-}
 
-        public static void main(String[] args) {
-          printing(Lambda::print_st);
-
-
-        }
+    public static void print_st(String t) {
+        System.out.println(t);
     }
 
-    interface SomeInt{
-        <T>void print(T t);
+    public static void print_st2(int t) {
+        System.out.println(t);
     }
+
+
+    static <T> void printing(SomeInt<T> si, T t) {
+        si.print(t);
+    }
+
+    public static void main(String[] args) {
+
+        printing(Lambda::print_st, "ghk");
+        printing(Lambda::print_st2, 100);
+
+    }
+}
+
+interface SomeInt<T> {
+    void print(T t);
+}
 
 
