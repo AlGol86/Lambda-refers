307e2f986ae71707f57e8b599301f227425beb90
Aleksey Golovenchik <goloven4ik@googlemail.com>
2021-03-08 21:34:41 +0300

    1

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,14 +1,20 @@
 public class Lambda {
-
+public static <T>void print_st(T t){
+    System.out.println(t);
+}
+static void printing(SomeInt si){
+    si.print(5);
+}
 
         public static void main(String[] args) {
-
+          printing(Lambda::print_st);
 
 
         }
     }
+
     interface SomeInt{
-        void print();
+        <T>void print(T t);
     }
 
 
