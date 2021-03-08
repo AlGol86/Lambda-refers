922ba160f1913d074da130028363c66589bb40d3
Aleksey Golovenchik <goloven4ik@googlemail.com>
2021-03-08 21:24:11 +0300

    init1

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,2 +1,39 @@
 public class Lambda {
-}
+
+        public static void print0(){
+            System.out.println("stat");
+        }
+        public void print1(){
+            System.out.println("instance");
+        }
+        private static void printing(SomeInt si){
+            si.print();
+        }
+
+        //for class method ref (tricky!)
+        private static void printing2(SomeInt2 si,Lambda le){
+            si.print(le);
+        }
+
+        public static void main(String[] args) {
+            printing(()-> System.out.println("LAMBDA"));
+            ////static method ref
+            printing(Lambda::print0);
+            ///instance method ref
+            Lambda le=new Lambda();
+            printing(le::print1);
+
+            ///instance method via class method ref (tricky!)
+            printing2(Lambda::print1,le);
+
+
+        }
+    }
+    interface SomeInt{
+        void print();
+    }
+
+    //for class method ref (tricky!)
+    interface SomeInt2{
+        void print(Lambda le);
+    }
